f3372f2e2996f29c516cb972b5f392cbc97e9c45
Hardik <hardik@wecreate.app>
2026-01-02 09:31:23 +0530

    adding status

--- src/controllers/transaction.controller.ts
+++ src/controllers/transaction.controller.ts
@@ -3,14 +3,19 @@ import { Request, Response } from "express";
 import ITransaction from "../types/transaction";
 import { log } from "node:console";
 
+const statuses = ["Scheduled", "Processing", "Paid", "Failed", "Cancelled"];
+
 const createTransaction = (req: Request, res: Response) => {
     log("HEADERS:", req.headers);
     log("BODY", req.body);
-    
+
+    const randomIndex = Math.floor(Math.random() * statuses.length);
+    const status = statuses[randomIndex];
+
     const transaction: ITransaction = {
         id: crypto.randomUUID(),
         createdAt: new Date(),
-        ...req.body
+        ...req.body?.invoices.map((invoice: any) => ({ ...invoice, status })),
     };
 
     return res.status(201).json({
