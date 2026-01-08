ff912450202b8b6631705e2e68088cbebc193fd8
Hardik <hardik@wecreate.app>
2026-01-08 19:27:03 +0530

    putting back cors

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 import express from "express";
-//import cors from "cors";
+import cors from "cors";
 import routes from "./routes/routes";
 
 const app = express();
 
-/* app.use(cors({
+app.use(cors({
     origin: "*", // change in production
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"]
-})); */
+}));
 
 app.use(express.json());
 
