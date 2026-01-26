f77023010ebb79b9ed4d2341db01c91bf05558c9
Hardik <hardik@wecreate.app>
2026-01-26 12:57:32 +0530

    adding log

--- src/services/webhookService.ts
+++ src/services/webhookService.ts
@@ -1,5 +1,8 @@
+import { log } from "node:console";
+
 export default async function sendWebhook(BC_WEBHOOK_URL: string, payload: any, signature: string) {
     try {
+        log("Sending webhook to:", BC_WEBHOOK_URL);
         const response = await fetch(BC_WEBHOOK_URL, {
             method: 'POST',
             headers: {
@@ -12,6 +15,8 @@ export default async function sendWebhook(BC_WEBHOOK_URL: string, payload: any, 
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`);
         }
+
+        log("Webhook sent successfully:", await response.text());
     } catch (err) {
         // In real life: retry + queue
         console.error('Webhook failed:', err);
