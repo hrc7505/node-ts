e9cf38a11867638200caf13fa7863a7d5ad22819
Hardik <hardik@wecreate.app>
2026-02-06 17:37:52 +0530

    updating route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -4,12 +4,13 @@ import pay from "../controllers/alphapay.controller";
 import makePayment from "../controllers/makePayment.controller";
 import connectChiizu from "../controllers/connectChiizu.controller";
 import disconnectChiizu from "../controllers/disconnectChiizu.controller";
+import schedulePayment from "../controllers/schedulePayment.controller copy";
 
 const router = Router();
 
 // BC
 router.post("/create-payment", makePayment);
-router.post("/schedule-payment", makePayment);
+router.post("/schedule-payment", schedulePayment);
 router.post("/connect-chiizu", connectChiizu);
 router.post("/disconnect-chiizu", disconnectChiizu);
 
