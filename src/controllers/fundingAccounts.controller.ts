0f0a608cb7e6bf16c5a83b4a6f18d0ca4eb54bfc
Hardik <hardik@wecreate.app>
2026-02-12 18:25:14 +0530

    returning random balances

--- src/controllers/fundingAccounts.controller.ts
+++ src/controllers/fundingAccounts.controller.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import { log } from "node:console";
 
+const createRandomBalance = () => Math.floor(Math.random() * 500000) + 10000;
+
 const fundingAccounts = async (req: Request, res: Response) => {
     log("funding-accounts headers::", req.headers);
 
@@ -13,21 +15,24 @@ const fundingAccounts = async (req: Request, res: Response) => {
                 name: "Main Operations Account",
                 accountNumber: "1234567890",
                 accountType: "Checking",
-                currencyCode: "USD"
+                currencyCode: "USD",
+                balance: createRandomBalance(),
             },
             {
                 id: "ACC-002",
                 name: "Secondary Savings",
                 accountNumber: "0987654321",
                 accountType: "Savings",
-                currencyCode: "USD"
+                currencyCode: "USD",
+                balance: createRandomBalance(),
             },
             {
                 id: "ACC-003",
                 name: "International Fund",
                 accountNumber: "5566778899",
                 accountType: "Checking",
-                currencyCode: "CAD"
+                currencyCode: "CAD",
+                balance: createRandomBalance(),
             }
         ]
     };
