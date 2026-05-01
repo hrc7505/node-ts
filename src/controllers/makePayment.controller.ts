6518f15fa2d72f6e9d764e07ff5f40c93295061f
Hardik <hardik@wecreate.app>
2026-05-01 19:38:56 +0530

    fixing issue

--- src/controllers/makePayment.controller.ts
+++ src/controllers/makePayment.controller.ts
@@ -33,7 +33,7 @@ const makePayment = async (req: Request<any, any, RequestBody>, res: Response) =
 
     await resend.emails.send({
         from: 'onboarding@resend.dev',
-        to: 'hardik@wecreate.dev',
+        to: 'hardik@wecreate.app',
         subject: 'Payment Initiated',
         html: `<div style="font-family: sans-serif; max-width: 600px; margin: auto;">
                     <h2 style="color: #333;">Payment Initiated</h2>
